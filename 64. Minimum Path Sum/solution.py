48f84fea7c1ee2ffc83cd5b3179bba2bac52c29f
Siddhant <siddhant.jain@timesinternet.in>
2026-08-09 23:12:33 +0530

    64 solution

---
+++ 64. Minimum Path Sum/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        
+        m = len(grid)
+        n = len(grid[0])
+        arr = [[0] * (n+1) for _ in range(m+1)]
+
+        arr[1][1] = grid[0][0]
+
+        for i in range(2, m+1):
+            arr[i][1] = arr[i-1][1] + grid[i-1][0]
+
+        for i in range(2, n+1):
+            arr[1][i] = arr[1][i-1] + grid[0][i-1]
+
+        for i in range(2, m+1):
+            for j in range(2, n+1):
+                arr[i][j] = min(arr[i-1][j], arr[i][j-1]) + grid[i-1][j-1]
+
+        return arr[m][n]
+
+
+
+        
