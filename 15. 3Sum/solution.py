43189f71ace2a4addeaf0656bc29e29da1c85b0d
Siddhant <siddhant.jain@timesinternet.in>
2026-08-09 23:33:48 +0530

    15 solution

---
+++ 15. 3Sum/solution.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        output = []
+        output_map = defaultdict(list)
+
+        for i in range(len(nums) - 1):
+            seen = {}
+            target = 0 - nums[i]
+            for j in range(i+1, len(nums)):
+                compliment = target - nums[j]
+                if compliment in seen:
+                    triplet = [nums[i],nums[j],compliment]
+                    sorted_triplet = tuple(sorted(triplet))
+                    output_map[sorted_triplet].append(triplet)
+                    # output.append([nums[i],nums[j],compliment])
+                seen[nums[j]] = j
+
+        for key in output_map.keys():   
+            output.append(key) 
+
+        return output
