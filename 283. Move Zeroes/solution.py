a919d1df33a37b9a23f29658a57523f4eb8c472c
Siddhant <siddhant.jain0607@gmail.com>
2026-08-11 23:27:50 +0530

    283 solution

---
+++ 283. Move Zeroes/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] == 0 and nums[j] != 0:
+                    nums[i], nums[j] = nums[j], nums[i]
+        
+        return nums
