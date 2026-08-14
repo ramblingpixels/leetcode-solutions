ed65a4985fdb06b2df0f967c01ea3380af95db79
Siddhant <siddhant.jain0607@gmail.com>
2026-08-14 23:40:44 +0530

    739 solution

---
+++ 739. Daily Temperatures/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        n = len(temperatures)
+        result = [0]*n
+
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev = stack.pop()
+                result[prev] = i - prev
+            stack.append(i)
+        
+        return result
+            
+
+                
