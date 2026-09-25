679aa4f7b7da00ef745b95cf257be6bb24f43ce9
Siddhant <siddhant.jain0607@gmail.com>
2026-09-25 22:24:04 +0530

    1025 solution update

--- 1025. Divisor Game/solution.py
+++ 1025. Divisor Game/solution.py
@@ -1,3 +1,14 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        return n % 2 == 0
+        dp = [False]*(n+1)
+        
+
+        for i in range(2, n+1):
+            for x in range(1, i):
+                if i%x == 0 and not dp[i-x]:
+                    dp[i] = True
+                    break
+
+        return dp[n]
+
+        # return n % 2 == 0
